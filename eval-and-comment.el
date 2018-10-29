08a060304521a6b17e32fe763c930dc2a16ae68d
David Wickes <dave.wickes@gmail.com>
2018-10-29 16:17:32 +0000

    extra packages for portacle setup

---
+++ eval-and-comment.el
@@ -0,0 +1,7 @@
+;; either eval the whole file (M-x eval-buffer) or each expression 
+(package-refresh-contents)
+(package-install 'lispy)
+(package-install 'default-text-scale)
+(default-text-scale-mode)
+(lispy-mode)
+(global-set-key (kbd "C-c C-e") 'lispy-eval-and-comment)
