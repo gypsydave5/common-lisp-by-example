a19c593bf936bad0d3b022f74ca3ee585664ed99
David Wickes <dave.wickes@gmail.com>
2018-11-08 14:38:11 +0000

    missing stuff put back in

--- lists.lisp
+++ lists.lisp
@@ -19,8 +19,51 @@
 (first (last (list "Chris" "Alex" "Dave")))
 ;; => "Dave"
 
+(rest (list 1 2 3))
+;; => (2 3)
+
 (third (list 1 2))
 ;; => NIL
 
 (nth 3 (list 3 4 5 6))
+;; => 6
+
+(append (list 1 2 3) (list 4 5 6))
+;; => (1 2 3 4 5 6)
+
+(butlast (list 1 2 3))
+;; => (1 2)
+
+(subseq (list "a" "b" "c" "d" "e" "f") 1 3)
+;; => ("b" "c")
+
+(subseq (list "a" "b" "c" "d" "e" "f") 3)
+;; => ("d" "e" "f")
+
+(subseq (list 1 2 3) 0 1337)
+;; WILL BLOW UP WITH LEETNESS
+
+(length (list 1 2 3 4 5 6))
+;; => 6
+
+(reverse (list 6 5 4 3 2 1))
+;; => (1 2 3 4 5 6)
+
+
+
+
+
+
+;; ELITE LEVEL
+
+(car (list 1 2 3))
+;; => 1
+
+(cdr (list 1 2 3))
+;; => (2 3)
+
+(caddar (list (list 1 2 3 4) (list 5 6 7 8)))
+;; => 3
+
+(cadadr (list (list 1 2 3 4) (list 5 6 7 8)))
 ;; => 6
